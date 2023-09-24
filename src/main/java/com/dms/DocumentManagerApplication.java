26edd47adee47488459db78576484ca79a4bb58f
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-24 21:05:51 +0200

    init

---
+++ src/main/java/com/dms/DocumentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dms/DocumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

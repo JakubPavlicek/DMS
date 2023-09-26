4932ae6e9a066f6417d7f4b700e1f5e239d38024
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-26 21:05:50 +0200

    switch to YAML

--- src/main/java/com/dms/DocumentManagerApplication.java
+++ src/main/java/com/dms/DocumentManagerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DocumentManagerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DocumentManagerApplication.class, args);
     }
-
 }

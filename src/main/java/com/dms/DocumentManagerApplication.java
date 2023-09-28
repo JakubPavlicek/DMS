3bf20580d6ec520073582431f8649584b3fd4ca7
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-28 16:27:56 +0200

    add support for envers repositories

--- src/main/java/com/dms/DocumentManagerApplication.java
+++ src/main/java/com/dms/DocumentManagerApplication.java
@@ -2,8 +2,10 @@ package com.dms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.config.EnableEnversRepositories;
 
 @SpringBootApplication
+@EnableEnversRepositories
 public class DocumentManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocumentManagerApplication.class, args);

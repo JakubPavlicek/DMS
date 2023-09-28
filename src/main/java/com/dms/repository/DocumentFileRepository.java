6fc26127635ca839d0babc064c29f3a04657fa25
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-28 16:26:56 +0200

    add update file path

--- src/main/java/com/dms/repository/DocumentFileRepository.java
+++ src/main/java/com/dms/repository/DocumentFileRepository.java
@@ -1,10 +1,17 @@
 package com.dms.repository;
 
 import com.dms.entity.DocumentFile;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentFileRepository extends JpaRepository<DocumentFile, String> {
-
+public interface DocumentFileRepository extends JpaRepository<DocumentFile, String>, RevisionRepository<DocumentFile, String, Long> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE DocumentFile file SET file.filePath = :location WHERE file.id = :id")
+    void updateDocumentFileLocationById(String id, String location);
 }

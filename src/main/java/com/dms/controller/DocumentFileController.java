6ac54533a4db8efde2de306e01c93f0d9e2ed69e
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-28 21:31:57 +0200

    add switch between revisions

--- src/main/java/com/dms/controller/DocumentFileController.java
+++ src/main/java/com/dms/controller/DocumentFileController.java
@@ -1,17 +1,17 @@
 package com.dms.controller;
 
 import com.dms.entity.DocumentFile;
+import com.dms.model.DocumentFileRequest;
 import com.dms.service.DocumentFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/files")
@@ -25,17 +25,23 @@ public class DocumentFileController {
     }
 
     @PostMapping("/upload")
-    public DocumentFile saveDocumentFile(@RequestBody MultipartFile file) {
-        return documentFileService.saveFile(file);
+    public DocumentFile saveDocumentFile(@ModelAttribute DocumentFileRequest fileRequest) {
+        return documentFileService.saveDocumentFile(fileRequest);
     }
 
     @GetMapping("/download/{id}")
-    public DocumentFile downloadDocumentFile(@PathVariable("id") String id) {
+    public DocumentFile getDocumentFile(@PathVariable("id") String id) {
         return documentFileService.getDocumentFile(id);
     }
 
     @PutMapping("/update/{id}")
     public String updateDocumentFile(@PathVariable("id") String id, @RequestBody DocumentFile file) {
         return documentFileService.updateDocumentFile(id, file);
+    }
+
+    @PutMapping("/{id}/revision/{revision}")
+    public DocumentFile setDocumentFileAsCurrent(@PathVariable("id") String id, @PathVariable("revision") Long revision)
+    {
+        return documentFileService.setDocumentFileAsCurrent(id, revision);
     }
 }

d268c972f60844302912665529167bbab639ec0b
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-29 17:44:48 +0200

    refactoring

--- src/main/java/com/dms/entity/DocumentFile.java
+++ src/main/java/com/dms/entity/DocumentFile.java
@@ -13,13 +13,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.envers.Audited;
-import org.hibernate.envers.NotAudited;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Audited
 @Setter
 @Getter
 @NoArgsConstructor
@@ -33,16 +30,13 @@ public class DocumentFile {
     private String fileName;
     private String fileType;
     private String filePath;
+    private String author;
 
-    @NotAudited
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    @NotAudited
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-
-    private String author;
 
     @Lob
     @Column(length = Integer.MAX_VALUE)

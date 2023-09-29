9e48ddbad77cc85ddd3f20aa7c10ce9b9d35a83b
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-29 17:45:01 +0200

    add document file revision entity

---
+++ src/main/java/com/dms/entity/DocumentFileRevision.java
@@ -0,0 +1,57 @@
+package com.dms.entity;
+
+import com.dms.model.FileOperation;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DocumentFileRevision {
+    @Id
+    @SequenceGenerator(
+        name = "revision_generator",
+        sequenceName = "revision_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "revision_generator"
+    )
+    private Long revisionId;
+
+    private String fileId;
+
+    private String fileName;
+    private String fileType;
+    private String filePath;
+    private String author;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private FileOperation fileOperation;
+
+    @Lob
+    @Column(length = Integer.MAX_VALUE)
+    private byte[] data;
+}

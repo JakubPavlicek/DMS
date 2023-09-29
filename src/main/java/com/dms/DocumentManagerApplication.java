f9a7bbf53270c2997da13125dfdd239a29d58af5
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-29 17:46:12 +0200

    remove spring data envers annotation

--- src/main/java/com/dms/DocumentManagerApplication.java
+++ src/main/java/com/dms/DocumentManagerApplication.java
@@ -2,10 +2,8 @@ package com.dms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.envers.repository.config.EnableEnversRepositories;
 
 @SpringBootApplication
-@EnableEnversRepositories
 public class DocumentManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocumentManagerApplication.class, args);

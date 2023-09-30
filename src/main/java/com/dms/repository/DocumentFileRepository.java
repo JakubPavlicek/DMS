defb89c63e95fcd77d4995ac15335fc6cef2ff76
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-09-30 19:36:11 +0200

    change package of file operation

--- src/main/java/com/dms/repository/DocumentFileRepository.java
+++ src/main/java/com/dms/repository/DocumentFileRepository.java
@@ -1,7 +1,7 @@
 package com.dms.repository;
 
 import com.dms.entity.DocumentFile;
-import com.dms.model.FileOperation;
+import com.dms.entity.FileOperation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;

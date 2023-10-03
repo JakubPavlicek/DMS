acccd31c87c37b2f73626f66b23d089852cecd6c
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-03 18:17:51 +0200

    add hash pointer + make data @Transient

--- src/main/java/com/dms/entity/DocumentRevision.java
+++ src/main/java/com/dms/entity/DocumentRevision.java
@@ -1,14 +1,13 @@
 package com.dms.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -51,7 +50,8 @@ public class DocumentRevision {
     @Enumerated(EnumType.STRING)
     private DocumentOperation operation;
 
-    @Lob
-    @Column(length = Integer.MAX_VALUE)
+    private String hashPointer;
+
+    @Transient
     private byte[] data;
 }

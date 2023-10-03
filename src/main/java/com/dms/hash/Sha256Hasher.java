6392a12aa9d743958f211a0891d8ce623d6252f2
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-03 18:19:07 +0200

    add SHA-256 hash class

---
+++ src/main/java/com/dms/hash/Sha256Hasher.java
@@ -0,0 +1,23 @@
+package com.dms.hash;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Sha256Hasher {
+    public static String hashFile(MultipartFile file) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(file.getBytes());
+            return Base64.getUrlEncoder()
+                         .encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Hashovaci algoritmus neexistuje");
+        } catch (IOException e) {
+            throw new RuntimeException("Nepodarilo se ziskat data souboru: " + file.getName());
+        }
+    }
+}

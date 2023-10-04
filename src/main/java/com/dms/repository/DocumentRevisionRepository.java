3732914376592cf0a456d17092dee330de2ab787
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-04 18:12:06 +0200

    remove find method + refactoring

--- src/main/java/com/dms/repository/DocumentRevisionRepository.java
+++ src/main/java/com/dms/repository/DocumentRevisionRepository.java
@@ -4,12 +4,9 @@ import com.dms.entity.DocumentRevision;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DocumentRevisionRepository extends JpaRepository<DocumentRevision, String> {
-    Optional<DocumentRevision> findByDocumentIdAndRevisionId(String documentId, Long revisionId);
-
-    List<DocumentRevision> findAllByDocumentId(String documentId);
+    Optional<DocumentRevision> findByDocument_DocumentIdAndRevisionId(String documentId, Long revisionId);
 }

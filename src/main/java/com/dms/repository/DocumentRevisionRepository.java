edb9e87bb63e44ce384be38c9fcf7a0da78b0ea7
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-04 22:09:48 +0200

    add query methods

--- src/main/java/com/dms/repository/DocumentRevisionRepository.java
+++ src/main/java/com/dms/repository/DocumentRevisionRepository.java
@@ -1,12 +1,25 @@
 package com.dms.repository;
 
+import com.dms.entity.Document;
 import com.dms.entity.DocumentRevision;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DocumentRevisionRepository extends JpaRepository<DocumentRevision, String> {
     Optional<DocumentRevision> findByDocument_DocumentIdAndRevisionId(String documentId, Long revisionId);
+
+    @Query("SELECT MAX(revision.version) FROM DocumentRevision revision WHERE revision.document = :document")
+    Optional<Long> findLastRevisionVersionByDocument(Document document);
+
+    List<DocumentRevision> findAllByDocumentOrderByCreatedAtAsc(Document document);
+
+    @Modifying
+    @Query("UPDATE DocumentRevision revision SET revision.version = :version WHERE revision = :documentRevision")
+    void updateVersion(DocumentRevision documentRevision, Long version);
 }

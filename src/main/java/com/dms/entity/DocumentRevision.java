df41cf3ab04ed708cab71bbe587a6f41ecf028bd
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-04 18:11:44 +0200

    add User and Document relationship

--- src/main/java/com/dms/entity/DocumentRevision.java
+++ src/main/java/com/dms/entity/DocumentRevision.java
@@ -1,13 +1,19 @@
 package com.dms.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,32 +32,48 @@ import java.time.LocalDateTime;
 public class DocumentRevision {
     @Id
     @SequenceGenerator(
-        name = "revision_generator",
-        sequenceName = "revision_sequence",
+        name = "revision_id_generator",
+        sequenceName = "revision_id_sequence",
         allocationSize = 1
     )
     @GeneratedValue(
         strategy = GenerationType.SEQUENCE,
-        generator = "revision_generator"
+        generator = "revision_id_generator"
     )
     private Long revisionId;
 
-    private String documentId;
+    @OneToOne(
+        fetch = FetchType.EAGER,
+        cascade = CascadeType.PERSIST
+    )
+    @JoinColumn(
+        name = "user_id",
+        referencedColumnName = "userId",
+        unique = false
+    )
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "document_id",
+        referencedColumnName = "documentId"
+    )
+    @JsonIgnore
+    private Document document;
 
     private String name;
     private String extension;
     private String type;
     private String path;
-    private String author;
+    private String hashPointer;
 
     @CreationTimestamp
+    @Column(
+        nullable = false,
+        updatable = false
+    )
     private LocalDateTime createdAt;
 
     @Enumerated(EnumType.STRING)
     private DocumentOperation operation;
-
-    private String hashPointer;
-
-    @Transient
-    private byte[] data;
 }

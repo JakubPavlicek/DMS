14ec92b178b240f8081908088efdb9f50604c052
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-08 13:32:09 +0200

    change class name to Hasher only + add hashAlgorithm configurable value

--- src/main/java/com/dms/hash/Hasher.java
+++ src/main/java/com/dms/hash/Hasher.java
@@ -3,6 +3,7 @@ package com.dms.hash;
 import com.dms.exception.FileOperation;
 import com.dms.exception.FileOperationException;
 import com.dms.exception.HashAlgorithmNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,16 +13,19 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 @Component
-public class Sha256Hasher {
+public class Hasher {
+
+    @Value("${hash.algorithm}")
+    private String hashAlgorithm;
 
     public String hashFile(MultipartFile file) {
         try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            MessageDigest digest = MessageDigest.getInstance(hashAlgorithm);
             byte[] hash = digest.digest(file.getBytes());
             return Base64.getUrlEncoder()
                          .encodeToString(hash);
         } catch (NoSuchAlgorithmException e) {
-            throw new HashAlgorithmNotFoundException("Hashovaci algoritmus nebyl nalezen ci neexistuje");
+            throw new HashAlgorithmNotFoundException("Hashovaci algoritmus: " + hashAlgorithm + " nebyl nalezen ci neexistuje");
         } catch (IOException e) {
             throw new FileOperationException(FileOperation.READ, "Nepodarilo se ziskat data souboru: " + file.getName());
         }

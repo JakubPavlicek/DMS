215ec81dc3cdd27dbc28aa43f2c9e5cac9afa4db
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-08 20:12:21 +0200

    remove unneccessary method

--- src/main/java/com/dms/service/UserService.java
+++ src/main/java/com/dms/service/UserService.java
@@ -1,11 +1,7 @@
 package com.dms.service;
 
 import com.dms.entity.User;
-import com.dms.exception.UserParsingException;
 import com.dms.repository.UserRepository;
-import com.dms.request.DocumentRequest;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,14 +17,6 @@ public class UserService {
 
         return userRepository.findByUsernameAndEmail(username, email)
                              .orElseGet(() -> userRepository.save(user));
-    }
-
-    public User getUserFromRequest(DocumentRequest documentRequest) {
-        try {
-            return new ObjectMapper().readValue(documentRequest.getUser(), User.class);
-        } catch (JsonProcessingException e) {
-            throw new UserParsingException("Nepodarilo se ziskat uzivatele z JSONu: " + documentRequest.getUser());
-        }
     }
 
 }

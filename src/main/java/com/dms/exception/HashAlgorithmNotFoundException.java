7c40b1c81ea1d9a6d62e2d5f73d43394a810bf19
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-08 13:27:17 +0200

    add hash algorithm not found exception

---
+++ src/main/java/com/dms/exception/HashAlgorithmNotFoundException.java
@@ -0,0 +1,9 @@
+package com.dms.exception;
+
+public class HashAlgorithmNotFoundException extends RuntimeException {
+
+    public HashAlgorithmNotFoundException(String message) {
+        super(message);
+    }
+
+}

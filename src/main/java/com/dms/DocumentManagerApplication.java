4ae48bc99391c3b486855701ae4b9ce46d2e456c
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-11 16:47:34 +0200

    add @EnableConfigurationProperties

--- src/main/java/com/dms/DocumentManagerApplication.java
+++ src/main/java/com/dms/DocumentManagerApplication.java
@@ -1,11 +1,15 @@
 package com.dms;
 
+import com.dms.config.BlobStorageProperties;
+import com.dms.config.HashProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableConfigurationProperties({BlobStorageProperties.class, HashProperties.class})
 public class DocumentManagerApplication {
 
     public static void main(String[] args) {

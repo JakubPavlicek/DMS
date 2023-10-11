22bf3730af8b8d3a78779113c0741f7f3bbafc98
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-11 16:45:26 +0200

    add properties validation

---
+++ src/main/java/com/dms/config/BlobStorageProperties.java
@@ -0,0 +1,45 @@
+package com.dms.config;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Validated
+@ConfigurationProperties(prefix = "storage")
+public class BlobStorageProperties implements Validator {
+
+    @NotBlank
+    private final String path;
+
+    @Min(value = 1)
+    private final int directoryPrefixLength;
+
+    @ConstructorBinding
+    public BlobStorageProperties(String path, int directoryPrefixLength)
+    {
+        this.path = path;
+        this.directoryPrefixLength = directoryPrefixLength;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return BlobStorageProperties.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Path storagePath = Paths.get(path);
+
+        if(Files.notExists(storagePath))
+            errors.rejectValue("path", "Blob Storage Path Not Found", "Blob storage neexistuje");
+    }
+
+}
---
+++ src/main/java/com/dms/config/HashProperties.java
@@ -0,0 +1,39 @@
+package com.dms.config;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Validated
+@ConfigurationProperties(prefix = "hash")
+public class HashProperties implements Validator {
+
+    @NotBlank(message = "Hashovaci algoritmus musi byt uveden")
+    private final String algorithm;
+
+    @ConstructorBinding
+    public HashProperties(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return HashProperties.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            errors.rejectValue("algorithm", "Hash Algorithm Not Found", "Hashovaci algoritmus neexistuje");
+        }
+    }
+
+}

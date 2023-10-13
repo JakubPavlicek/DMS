6f2833d5da85a1b6fc621eb8b106086c0c4391c6
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-13 22:09:01 +0200

    add paging and sorting method + refactoring

--- src/main/java/com/dms/service/DocumentService.java
+++ src/main/java/com/dms/service/DocumentService.java
@@ -2,16 +2,21 @@ package com.dms.service;
 
 import com.dms.dto.DocumentDTO;
 import com.dms.dto.DocumentRevisionDTO;
+import com.dms.dto.SortFieldItem;
 import com.dms.dto.UserDTO;
 import com.dms.entity.Document;
 import com.dms.entity.DocumentRevision;
 import com.dms.entity.User;
-import com.dms.exception.RevisionNotFoundException;
 import com.dms.repository.DocumentRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -45,7 +50,19 @@ public class DocumentService {
                         .toList();
     }
 
-    private Document getDocumentFromFile(MultipartFile file) {
+    private User getUserFromUserDto(UserDTO userDto) {
+        User user = documentCommonService.mapUserDtoToUser(userDto);
+        return userService.getUser(user);
+    }
+
+    private Document createDocumentFromUserDtoAndFile(UserDTO userDto, MultipartFile file) {
+        String hash = documentCommonService.storeBlob(file);
+        User author = getUserFromUserDto(userDto);
+
+        return createDocument(file, hash, author);
+    }
+
+    private Document createDocument(MultipartFile file, String hash, User author) {
         String originalFileName = Objects.requireNonNull(file.getOriginalFilename());
         String path = StringUtils.cleanPath(originalFileName);
         String name = StringUtils.getFilename(path);
@@ -54,45 +71,29 @@ public class DocumentService {
         return Document.builder()
                        .name(name)
                        .type(type)
+                       .hash(hash)
+                       .author(author)
                        .build();
     }
 
     @Transactional
     public DocumentDTO saveDocument(UserDTO userDto, MultipartFile file) {
-        String hash = documentCommonService.storeBlob(file);
-
-        User user = documentCommonService.mapUserDtoToUser(userDto);
-        User author = userService.getUser(user);
-
-        Document document = getDocumentFromFile(file);
-        document.setHash(hash);
-        document.setAuthor(author);
+        Document document = createDocumentFromUserDtoAndFile(userDto, file);
 
         // flush to immediately initialize the "createdAt" and "updatedAt" fields, ensuring the DTO does not contain null values for these properties
         Document savedDocument = documentRepository.saveAndFlush(document);
 
-        documentCommonService.createDocumentRevision(savedDocument);
+        documentCommonService.saveRevisionFromDocument(savedDocument);
 
         return documentCommonService.mapDocumentToDocumentDto(savedDocument);
     }
 
     @Transactional
     public String updateDocument(String documentId, UserDTO userDto, MultipartFile file) {
-        Document databaseDocument = documentCommonService.getDocument(documentId);
-
-        String hash = documentCommonService.storeBlob(file);
-
-        User user = documentCommonService.mapUserDtoToUser(userDto);
-        User author = userService.getUser(user);
-
-        Document document = getDocumentFromFile(file);
+        Document document = createDocumentFromUserDtoAndFile(userDto, file);
         document.setDocumentId(documentId);
-        document.setCreatedAt(databaseDocument.getCreatedAt());
-        document.setHash(hash);
-        document.setAuthor(author);
 
-        documentCommonService.createDocumentRevision(document);
-
+        documentCommonService.saveRevisionFromDocument(document);
         documentRepository.save(document);
 
         return "Document updated successfully";
@@ -101,13 +102,7 @@ public class DocumentService {
     @Transactional
     public DocumentRevisionDTO switchToVersion(String documentId, Long version) {
         Document document = documentCommonService.getDocument(documentId);
-        List<DocumentRevision> revisions = document.getRevisions();
-
-        DocumentRevision revision = revisions.stream()
-                                             .filter(rev -> rev.getVersion()
-                                                               .equals(version))
-                                             .findFirst()
-                                             .orElseThrow(() -> new RevisionNotFoundException("Nebyla nalezena revize s verzi: " + version));
+        DocumentRevision revision = documentCommonService.getRevisionByDocumentAndVersion(document, version);
 
         documentCommonService.updateDocumentToRevision(document, revision);
 
@@ -136,6 +131,24 @@ public class DocumentService {
                              .contentType(MediaType.parseMediaType(document.getType()))
                              .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + document.getName() + "\"")
                              .body(new ByteArrayResource(data));
+    }
+
+    public Page<DocumentDTO> getDocumentsWithPagingAndSorting(int pageNumber, int pageSize, List<SortFieldItem> sortFieldItems) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+
+        if (Objects.nonNull(sortFieldItems)) {
+            Sort sort = documentCommonService.getSortFromFields(sortFieldItems);
+            pageable = PageRequest.of(pageNumber, pageSize, sort);
+        }
+
+        Page<Document> documents = documentRepository.findAll(pageable);
+        long totalDocuments = documentRepository.count();
+
+        List<DocumentDTO> documentDTOs = documents.stream()
+                                                  .map(documentCommonService::mapDocumentToDocumentDto)
+                                                  .toList();
+
+        return new PageImpl<>(documentDTOs, pageable, totalDocuments);
     }
 
 }

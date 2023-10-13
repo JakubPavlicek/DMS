e9d988107b037b15b6c6420b9fca53cade0e7fd0
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-13 12:13:12 +0200

    add blob and map methods + refactoring

--- src/main/java/com/dms/service/DocumentCommonService.java
+++ src/main/java/com/dms/service/DocumentCommonService.java
@@ -1,21 +1,29 @@
 package com.dms.service;
 
+import com.dms.dto.DocumentDTO;
+import com.dms.dto.DocumentRevisionDTO;
+import com.dms.dto.UserDTO;
 import com.dms.entity.Document;
 import com.dms.entity.DocumentRevision;
+import com.dms.entity.User;
 import com.dms.exception.DocumentNotFoundException;
 import com.dms.exception.RevisionNotFoundException;
 import com.dms.repository.DocumentRepository;
 import com.dms.repository.DocumentRevisionRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @AllArgsConstructor
-public class DocumentServiceCommon {
+public class DocumentCommonService {
 
     private final DocumentRepository documentRepository;
     private final DocumentRevisionRepository revisionRepository;
+
     private final BlobStorageService blobStorageService;
+    private final ModelMapper modelMapper;
 
     public Document getDocument(String documentId) {
         return documentRepository.findById(documentId)
@@ -25,15 +33,6 @@ public class DocumentServiceCommon {
     public DocumentRevision getRevision(Long revisionId) {
         return revisionRepository.findByRevisionId(revisionId)
                                  .orElseThrow(() -> new RevisionNotFoundException("Revize s ID: " + revisionId + " nebyla nalezena"));
-    }
-
-    private boolean isDuplicateHashPresent(String hash) {
-        return documentRepository.duplicateHashExists(hash) || revisionRepository.duplicateHashExists(hash);
-    }
-
-    public void deleteBlobIfDuplicateHashNotExists(String hash) {
-        if (!isDuplicateHashPresent(hash))
-            blobStorageService.deleteBlob(hash);
     }
 
     public void updateDocumentToRevision(Document document, DocumentRevision documentRevision) {
@@ -60,6 +59,35 @@ public class DocumentServiceCommon {
     private Long getLastRevisionVersion(Document document) {
         return revisionRepository.findLastRevisionVersionByDocument(document)
                                  .orElse(0L);
+    }
+
+    public String storeBlob(MultipartFile file) {
+        return blobStorageService.storeBlob(file);
+    }
+
+    public byte[] getBlob(String hash) {
+        return blobStorageService.getBlob(hash);
+    }
+
+    public void deleteBlobIfDuplicateHashNotExists(String hash) {
+        if (!isDuplicateHashPresent(hash))
+            blobStorageService.deleteBlob(hash);
+    }
+
+    private boolean isDuplicateHashPresent(String hash) {
+        return documentRepository.duplicateHashExists(hash) || revisionRepository.duplicateHashExists(hash);
+    }
+
+    public DocumentDTO mapDocumentToDocumentDto(Document document) {
+        return modelMapper.map(document, DocumentDTO.class);
+    }
+
+    public DocumentRevisionDTO mapRevisionToRevisionDto(DocumentRevision revision) {
+        return modelMapper.map(revision, DocumentRevisionDTO.class);
+    }
+
+    public User mapUserDtoToUser(UserDTO userDTO) {
+        return modelMapper.map(userDTO, User.class);
     }
 
 }

e1253fa30c66c325d244bf85c9759e29b458464d
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-14 18:43:44 +0200

    extend JpaSpecificationExecutor

--- src/main/java/com/dms/repository/DocumentRepository.java
+++ src/main/java/com/dms/repository/DocumentRepository.java
@@ -2,11 +2,12 @@ package com.dms.repository;
 
 import com.dms.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentRepository extends JpaRepository<Document, String> {
+public interface DocumentRepository extends JpaRepository<Document, String>, JpaSpecificationExecutor<Document> {
 
     @Query("SELECT COUNT(document.hash) > 1 FROM Document document WHERE document.hash = :hash")
     boolean duplicateHashExists(String hash);

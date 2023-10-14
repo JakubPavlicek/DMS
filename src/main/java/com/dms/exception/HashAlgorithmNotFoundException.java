fc02e81edd8926ef6a90cb39ea1487a2363ac72b
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-14 11:53:03 +0200

    remove hash exception

--- src/main/java/com/dms/exception/HashAlgorithmNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.dms.exception;
-
-public class HashAlgorithmNotFoundException extends RuntimeException {
-
-    public HashAlgorithmNotFoundException(String message) {
-        super(message);
-    }
-
-}

3c446e429fbc165564a5496bda6ff7c95b434b6d
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-15 17:31:53 +0200

    remove @EnableWebMvc

--- src/main/java/com/dms/DocumentManagerApplication.java
+++ src/main/java/com/dms/DocumentManagerApplication.java
@@ -5,10 +5,8 @@ import com.dms.config.HashPropertiesValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 @EnableConfigurationProperties({BlobStoragePropertiesValidator.class, HashPropertiesValidator.class})
 public class DocumentManagerApplication {
 

763bd2ae9f4071d8680810bd7a5aeb1c306c0c85
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-15 15:54:03 +0200

    make FilterItem POJO

--- src/main/java/com/dms/dto/FilterItem.java
+++ src/main/java/com/dms/dto/FilterItem.java
@@ -1,29 +1,13 @@
 package com.dms.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
-@NoArgsConstructor
+@Getter
 @AllArgsConstructor
 public class FilterItem {
 
-    private final String separator = ":";
-
-    @NotBlank(message = "A filtering item must be provided")
-    @Pattern(
-        regexp = "^[a-zA-Z]+:.+$",
-        message = "The format of filtering item does not match, the correct format is: <item>:<anything>"
-    )
-    private String fieldWithValue;
-
-    public String getField() {
-        return fieldWithValue.split(separator)[0];
-    }
-
-    public String getValue() {
-        return fieldWithValue.split(separator)[1];
-    }
+    private String field;
+    private String value;
 
 }

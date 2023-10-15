96428faa1c56bf966ef633b89a6dfc0a9f4bfbd0
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-15 15:54:17 +0200

    add InvalidRegexInputException

---
+++ src/main/java/com/dms/exception/InvalidRegexInputException.java
@@ -0,0 +1,9 @@
+package com.dms.exception;
+
+public class InvalidRegexInputException extends RuntimeException {
+
+    public InvalidRegexInputException(String message) {
+        super(message);
+    }
+
+}

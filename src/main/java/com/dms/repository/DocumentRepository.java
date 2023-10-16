1acbd0bc829eb70932cd3cf7bf82df666a25fa84
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-16 22:09:32 +0200

    add getCreatedAtByDocumentId method

--- src/main/java/com/dms/repository/DocumentRepository.java
+++ src/main/java/com/dms/repository/DocumentRepository.java
@@ -6,10 +6,16 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, String>, JpaSpecificationExecutor<Document> {
 
     @Query("SELECT COUNT(document.hash) > 1 FROM Document document WHERE document.hash = :hash")
     boolean duplicateHashExists(String hash);
+
+    @Query("SELECT document.createdAt FROM Document document WHERE document.documentId = :documentId")
+    Optional<LocalDateTime> getCreatedAtByDocumentId(String documentId);
 
 }

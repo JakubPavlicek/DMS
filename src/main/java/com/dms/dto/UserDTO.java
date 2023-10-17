d49ef89446c10622cfe8ae31e717df9afe047a40
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-17 15:40:27 +0200

    remove validation

--- src/main/java/com/dms/dto/UserDTO.java
+++ src/main/java/com/dms/dto/UserDTO.java
@@ -1,8 +1,5 @@
 package com.dms.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,14 +12,7 @@ import lombok.Setter;
 public class UserDTO {
 
     private Long userId;
-
-    @NotBlank(message = "Username is mandatory")
-    @Size(max = 40, message = "Username must not exceed 40 characters")
     private String username;
-
-    @NotBlank(message = "Email is mandatory")
-    @Size(max = 255, message = "Email must not exceed 255 characters")
-    @Email(message = "Invalid email format")
     private String email;
 
 }

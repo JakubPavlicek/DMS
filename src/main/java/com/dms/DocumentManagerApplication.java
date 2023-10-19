323182be76a3b3d3e547b4a5f945dad208bbbdab
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-19 15:57:35 +0200

    remove SpringProperties

--- src/main/java/com/dms/DocumentManagerApplication.java
+++ src/main/java/com/dms/DocumentManagerApplication.java
@@ -2,13 +2,12 @@ package com.dms;
 
 import com.dms.config.BlobStorageProperties;
 import com.dms.config.HashProperties;
-import com.dms.config.SpringProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({SpringProperties.class, BlobStorageProperties.class, HashProperties.class})
+@EnableConfigurationProperties({BlobStorageProperties.class, HashProperties.class})
 public class DocumentManagerApplication {
 
     public static void main(String[] args) {

cd74147a44e766b525d64300f372633ffc3aa336
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-26 22:45:09 +0200

    replace Long with UUID

--- src/main/java/com/dms/entity/User.java
+++ src/main/java/com/dms/entity/User.java
@@ -5,13 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "app_user")
@@ -23,20 +24,13 @@ import lombok.Setter;
 public class User {
 
     @Id
-    @SequenceGenerator(
-        name = "user_id_generator",
-        sequenceName = "user_id_sequence",
-        allocationSize = 1
-    )
-    @GeneratedValue(
-        strategy = GenerationType.SEQUENCE,
-        generator = "user_id_generator"
-    )
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(
+        length = 36,
         nullable = false,
         unique = true
     )
-    private Long userId;
+    private UUID userId;
 
     @Column(
         length = 40,

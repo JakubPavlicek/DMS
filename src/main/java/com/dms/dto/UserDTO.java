3e6e80eebf4bc1581208ea8b3970189cc737969b
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-26 22:45:02 +0200

    replace Long with UUID

--- src/main/java/com/dms/dto/UserDTO.java
+++ src/main/java/com/dms/dto/UserDTO.java
@@ -5,13 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
 
-    private Long userId;
+    private UUID userId;
     private String username;
     private String email;
 

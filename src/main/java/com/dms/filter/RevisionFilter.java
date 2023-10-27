dc642b86fd5623aac999e84799d7d1fbb786adec
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-27 16:28:11 +0200

    add Filter classes

---
+++ src/main/java/com/dms/filter/RevisionFilter.java
@@ -0,0 +1,25 @@
+package com.dms.filter;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class RevisionFilter {
+
+    // valid filter format (comma-separated): <field>:<value> -> group 1: field, group 2: value
+    public static final String FILTER_REGEX = "(" + getFieldOptions() + "):\"([^,]*)\"(?:,|$)";
+
+    // valid filter fields
+    private enum Field {
+        NAME,
+        TYPE,
+        PATH
+    }
+
+    private static String getFieldOptions() {
+        return Arrays.stream(Field.values())
+                     .map(Enum::name)
+                     .map(String::toLowerCase)
+                     .collect(Collectors.joining("|"));
+    }
+
+}

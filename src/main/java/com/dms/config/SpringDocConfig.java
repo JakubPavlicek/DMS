650a5ab150bd5e671317526352e98fcd10e23039
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-10-30 19:51:04 +0100

    fix file uploading in swagger-ui

--- src/main/java/com/dms/config/SpringDocConfig.java
+++ src/main/java/com/dms/config/SpringDocConfig.java
@@ -7,11 +7,20 @@ import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springdoc.core.providers.ObjectMapperProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Configuration
 public class SpringDocConfig {
+
+    public SpringDocConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+    }
 
     @Bean
     SpringDocConfiguration springDocConfiguration() {

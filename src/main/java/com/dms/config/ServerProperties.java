c6a8364c9ef8296b7df323fd78aa79d80574d7d4
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-11-03 15:13:49 +0100

    add ServerProperties

---
+++ src/main/java/com/dms/config/ServerProperties.java
@@ -0,0 +1,21 @@
+package com.dms.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@Getter
+@ConfigurationProperties(prefix = "server")
+public class ServerProperties {
+
+    @Value("${server.error.path}")
+    private String errorPath;
+
+    public String getErrorUrl() {
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                                          .path(errorPath)
+                                          .toUriString();
+    }
+
+}

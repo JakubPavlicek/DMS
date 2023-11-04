9290cb2af1c0640e9bf3df105fd13a1044f21c47
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-11-04 11:23:07 +0100

    add logging

--- src/main/java/com/dms/hash/Hasher.java
+++ src/main/java/com/dms/hash/Hasher.java
@@ -4,26 +4,33 @@ import com.dms.config.HashProperties;
 import com.dms.exception.FileOperation;
 import com.dms.exception.FileOperationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.apache.tomcat.util.buf.HexUtils;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.security.MessageDigest;
 
-@Component
+@Service
+@Log4j2
 @RequiredArgsConstructor
 public class Hasher {
 
     private final HashProperties hashProperties;
 
     public String hashFile(MultipartFile file) {
+        String filename = file.getOriginalFilename();
+        log.info("Hashing file: '{}'", filename);
+
         try {
             MessageDigest digest = MessageDigest.getInstance(hashProperties.getAlgorithm());
             byte[] hash = digest.digest(file.getBytes());
 
             return HexUtils.toHexString(hash);
         } catch (Exception e) {
-            throw new FileOperationException(FileOperation.READ, "Nepodarilo se ziskat data souboru: " + file.getName());
+            String message = "Failed to retrieve data from file: " + filename;
+            log.error(message, e);
+            throw new FileOperationException(FileOperation.READ, message);
         }
     }
 

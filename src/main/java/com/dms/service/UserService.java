884eaf20b07d9b31a67f39550b10b4a5fce7e693
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-11-07 22:28:22 +0100

    implement UserDetailsService + add methods

--- src/main/java/com/dms/service/UserService.java
+++ src/main/java/com/dms/service/UserService.java
@@ -1,26 +1,61 @@
 package com.dms.service;
 
+import com.dms.dto.UserDTO;
 import com.dms.entity.User;
+import com.dms.mapper.dto.UserDTOMapper;
 import com.dms.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public User getSavedUser(User user) {
-        log.debug("Getting saved user: user={}", user);
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
 
-        String username = user.getUsername();
-        String email = user.getEmail();
+    private final TokenService tokenService;
 
-        return userRepository.findByUsernameAndEmail(username, email)
-                             .orElseGet(() -> userRepository.save(user));
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                             .orElseThrow(() -> new RuntimeException("User with email: " + username + " not found"));
+    }
+
+    public UserDTO register(UserDTO userDTO) {
+        User user = UserDTOMapper.mapToUser(userDTO);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        User savedUser = userRepository.save(user);
+        return UserDTOMapper.mapToUserDTO(savedUser);
+    }
+
+    public String token(UserDTO user) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+        return tokenService.generateToken(authentication);
+    }
+
+    private String getAuthenticatedUserEmail() {
+        return SecurityContextHolder.getContext()
+                                    .getAuthentication()
+                                    .getName();
+    }
+
+    public User getAuthenticatedUser() {
+        String authUserEmail = getAuthenticatedUserEmail();
+        return userRepository.findByEmail(authUserEmail)
+                             .orElseThrow(() -> new RuntimeException("User with email: " + authUserEmail + " not found"));
     }
 
 }

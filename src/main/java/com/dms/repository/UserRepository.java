f23cfdb9a05049f977a573107214aa91cf3fe102
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-11-08 21:17:33 +0100

    add existsByEmail

--- src/main/java/com/dms/repository/UserRepository.java
+++ src/main/java/com/dms/repository/UserRepository.java
@@ -11,6 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
-    Optional<User> findByUsernameAndEmail(String username, String email);
+    boolean existsByEmail(String email);
 
 }

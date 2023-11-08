14916f5763e47eaa3d3038f02ebb6b92a09f1d08
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-11-08 17:21:56 +0100

    add createUser, deleteUser, getCurrentUser, updateUser methods

--- src/main/java/com/dms/service/UserService.java
+++ src/main/java/com/dms/service/UserService.java
@@ -6,9 +6,6 @@ import com.dms.mapper.dto.UserDTOMapper;
 import com.dms.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,27 +20,12 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
-
-    private final TokenService tokenService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
                              .orElseThrow(() -> new RuntimeException("User with email: " + username + " not found"));
-    }
-
-    public UserDTO register(UserDTO userDTO) {
-        User user = UserDTOMapper.mapToUser(userDTO);
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        User savedUser = userRepository.save(user);
-        return UserDTOMapper.mapToUserDTO(savedUser);
-    }
-
-    public String token(UserDTO user) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
-        return tokenService.generateToken(authentication);
     }
 
     private String getAuthenticatedUserEmail() {
@@ -56,6 +38,27 @@ public class UserService implements UserDetailsService {
         String authUserEmail = getAuthenticatedUserEmail();
         return userRepository.findByEmail(authUserEmail)
                              .orElseThrow(() -> new RuntimeException("User with email: " + authUserEmail + " not found"));
+    }
+
+    public UserDTO createUser(UserDTO userDTO) {
+        User user = UserDTOMapper.mapToUser(userDTO);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        User savedUser = userRepository.save(user);
+        return UserDTOMapper.mapToUserDTO(savedUser);
+    }
+
+    public void deleteUser(String userId) {
+        // TODO: delete user and all of his documents
+    }
+
+    public UserDTO getCurrentUser() {
+        User authenticatedUser = getAuthenticatedUser();
+        return UserDTOMapper.mapToUserDTO(authenticatedUser);
+    }
+
+    public UserDTO updateUser(String userId, UserDTO userDTO) {
+        // TODO: update credentials - email has to be unique
+        return null;
     }
 
 }

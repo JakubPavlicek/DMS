b0cce15c43cd87f4b1ee6e69139c88b83f3f3a5d
Jakub Pavlíček <kuba.cb@seznam.cz>
2023-11-10 12:14:28 +0100

    add logs

--- src/main/java/com/dms/service/AuthService.java
+++ src/main/java/com/dms/service/AuthService.java
@@ -3,6 +3,7 @@ package com.dms.service;
 import com.dms.dto.TokenResponseDTO;
 import com.dms.dto.UserLoginDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,6 +17,7 @@ import java.time.temporal.ChronoUnit;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
@@ -23,6 +25,9 @@ public class AuthService {
 
     public TokenResponseDTO token(UserLoginDTO userLogin) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLogin.getEmail(), userLogin.getPassword()));
+
+        log.info("User {} authenticated successfully", userLogin.getEmail());
+
         String token = generateToken(authentication);
 
         return TokenResponseDTO.builder()
--- src/main/java/com/dms/util/KeyGenerator.java
+++ src/main/java/com/dms/util/KeyGenerator.java
@@ -1,15 +1,20 @@
 package com.dms.util;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 
+@Log4j2
 public class KeyGenerator {
 
     public static KeyPair generateRsaKeys() {
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             keyPairGenerator.initialize(2048);
-            return keyPairGenerator.generateKeyPair();
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            log.info("Successfully generated key pair");
+            return keyPair;
         } catch (Exception e) {
             throw new RuntimeException("RSA key pair couldn't be created");
         }
